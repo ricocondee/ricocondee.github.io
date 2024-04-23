014d1aee6d58ae142bd7818c2cff9643bdce41ca
Emanuel Rico Conde <ricocondee@gmail.com>
2024-04-22 20:21:47 -0500

    styles, layout and required files

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,26 @@
-import Header from '../components/Header';
+import PropTypes from "prop-types";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Modal from "../containers/Modal";
+import { useState } from "react";
 
 const Layout = ({ children }) => {
-	return (
-		<div className="Layout">
-			<Header />
-			{children}
-		</div>
-	);
-}
+  const [modal, setModal] = useState(false);
+  const handleModal = () => {
+    setModal(!modal);
+  };
+  return (
+    <div className="Layout">
+      <Header funcBtn={handleModal}/>
+      {children}
+      <Footer />
+      {modal && <Modal func={handleModal} />}
+    </div>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Layout;
