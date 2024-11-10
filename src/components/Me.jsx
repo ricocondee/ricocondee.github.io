2270a6d6277b16a1c32f421f4e73ba318a38d6f9
Emanuel Rico Conde <ricocondee@gmail.com>
2024-11-10 13:26:27 -0500

    looking for a better way to display projects
    Signed-off-by: Emanuel Rico Conde <ricocondee@gmail.com>

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import mySelf from "../assets/me.jpg";
+import mySelfNBG from "../assets/meNoBG.png";
 
 const Me = () => {
   return (
     <div className="myself">
     <img src={mySelf} alt="Emanuel Rico Conde" />
+    <img src={mySelfNBG} className="to__border" alt="" />
   </div>
   )
 }
